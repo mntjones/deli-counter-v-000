419a53b31653f547ab6d86a28de3019d00e98171
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-02 21:33:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,14 @@ katz_deli = []
 def line(queue)
   if queue.empty?
     puts "The line is currently empty."
+    break
   else
     str = "The line is currently "
     queue.each_with_index do |person, index|
-      puts "#{index +1}. #{person} "
+      str += "#{index +1}. #{person} "
     end
   end
+  str
 end
 
 def take_a_number(queue)
